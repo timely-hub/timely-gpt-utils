6b95005c7c94c397d4842e4a284fcd49f2b84535
dave <eightymountain@gmail.com>
2026-03-29 17:17:51 +0900

    test: subject 테스트

--- src/test.ts
+++ src/test.ts
@@ -7,6 +7,14 @@ export const test = () => {
     userId: "1234567890",
   });
   console.log("combineFolderPath", test1);
+  const test11 = filePathUtils.combineFolderPath("protected", {
+    orgScope: "timely-gpt",
+    spaceId: "1234567890",
+    userId: "1234567890",
+    subjects: [["group", "group-id123"]],
+    spaceMemberId: "smid_191292",
+  });
+  console.log("combineFolderPath", test11);
 
   const test2 = filePathUtils.combineFilePath(
     "protected",
@@ -18,6 +26,20 @@ export const test = () => {
     ".txt",
   );
   console.log("combineFilePath", test2);
+
+  const test3 = filePathUtils.combineFilePath(
+    "protected",
+    {
+      orgScope: "timely-gpt",
+      spaceId: "sid_19213",
+      userId: "uid_128723",
+      subjects: [["group", "group-id123"]],
+      purpose: "CHAT",
+      spaceMemberId: "smid_191292",
+    },
+    ".txt",
+  );
+  console.log("combineFilePath", test3);
 };
 
 test();
